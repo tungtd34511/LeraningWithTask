e4c5d0185426d9b716661050760f969ffce61dcf
Tùng Tạ Duy <taduytung24112000@gmail.com>
2023-07-19 12:13:59 +0700

    Add project files.

---
+++ LeraningWithTask/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(DateTime.Now);
+await Task.Delay(5000);
+await Task.Run(() =>
+{
+    Console.WriteLine(DateTime.Now);
+});
